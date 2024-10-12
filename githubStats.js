3036712e066107c6b4e235f698cc4e5e1301a767
mariemBenRh <122294772+Mbenrh@users.noreply.github.com>
2024-10-12 01:48:04 +0100

    Update githubStats.js

--- githubStats.js
+++ githubStats.js
@@ -1,24 +1,34 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    const token = process.env.GITHUB_TOKEN; / 
+    const token = process.env.GITHUB_TOKEN;
     const username = req.query.username || 'Mbenrh';
     
-    const statsResponse = await fetch(`https://github-readme-stats.vercel.app/api?username=${username}&include_all_commits=true&count_private=true`, {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
-    
-    const langResponse = await fetch(`https://github-readme-stats.vercel.app/api/top-langs?username=${username}&include_all_commits=true&count_private=true`, {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
+    try {
+        const statsResponse = await fetch(`https://github-readme-stats.vercel.app/api?username=${username}&include_all_commits=true&count_private=true`, {
+            headers: {
+                Authorization: `token ${token}`
+            }
+        });
+        
+        const langResponse = await fetch(`https://github-readme-stats.vercel.app/api/top-langs?username=${username}&include_all_commits=true&count_private=true`, {
+            headers: {
+                Authorization: `token ${token}`
+            }
+        });
 
-    const stats = await statsResponse.text();
-    const langStats = await langResponse.text();
+        if (!statsResponse.ok || !langResponse.ok) {
+            throw new Error('Failed to fetch data from GitHub');
+        }
 
-    res.setHeader('Content-Type', 'text/plain');
-    res.send(stats + '\n' + langStats);
+        const stats = await statsResponse.text();
+        const langStats = await langResponse.text();
+
+        res.setHeader('Content-Type', 'text/plain');
+        res.send(stats + '\n' + langStats);
+    } catch (error) {
+        console.error('Error fetching GitHub stats:', error);
+        res.status(500).send('Internal Server Error');
+    }
 };
+ 
